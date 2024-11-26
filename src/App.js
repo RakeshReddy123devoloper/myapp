91d87dba2ca7f5f9309bba9430c5f7c043e694db
RakeshReddy123devoloper <aanumandlarakeshreddy@gmail.com>
2024-11-26 15:25:03 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React js in 10k coders
         </a>
       </header>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+// import Sample from './sample';
+// import Time from './timetable';
+import Img from './img'
+import Header from './header'
+import Footer from './footer'
+// import Main from './mobile'
+// //  import Main from './fakestore';
+//  import Fresher from './fresherjob';
+// import Parent from './propsparent'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    {/* <Sample/> */}
+    {/* <Time/> */}
+   
+     <Header/> 
+      <Img/> 
+    <Footer/>
+    {/* <Main/> */}
+    
+    {/* <Fresher/> */}
+    {/* <Parent/> */}
   </React.StrictMode>
 );
 
